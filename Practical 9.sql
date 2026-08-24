68c860c7ef82523c703c226a936c2d1ccf45c593
Pushkar Poman <pushkarpoman10@gmail.com>
2026-08-24 20:13:48 +0530

    Add files via upload

---
+++ Practical 9.sql
@@ -0,0 +1,12 @@
+create database classroom;
+use classroom;
+CREATE TABLE Student( 
+Stud_ID INT PRIMARY KEY, 
+Stud_Name VARCHAR(255));
+
+
+-- After code execution
+SELECT * FROM Student;
+
+SELECT * FROM Student;
+SELECT * FROM Student;
